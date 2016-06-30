e67f96ad4b8e0cabfe4211a4305156ce2922d5bf
Valentin <valentinsusset@hotmail.com>
2016-06-30 16:45:18 +0200

    fonctions compteur, multiplicateur et -50

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 var score = 0;
+var compteur = 1;
+
+function incrementer() {
+	document.getElementById("nbreclics").innerHTML="nombre de multiplicateurs : " + compteur ++;	
+};
+
+function prix() {
+	score -= 50;
+	document.getElementById("affichage").innerHTML = score;
+}
+
+function point() {
+	document.getElementById("affichage").innerHTML = score += compteur; 
+};
+
+
+
+
+
