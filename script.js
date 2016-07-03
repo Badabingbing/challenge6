75a4a58f06840690878c1c86e5dcfea06ab57450
Valentin <valentinsusset@hotmail.com>
2016-07-03 18:12:40 +0200

    etape 8

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ var score = 0;
 var compteur = 1;
 
 function incrementer() {
-	document.getElementById("nbreclics").innerHTML="nombre de multiplicateurs : " + compteur ++;	
-};
-
-function prix() {
-	score -= 50;
+	if (score >= 50) {score -= 50; compteur ++}
+	else {alert("Il te faut au moins 50 points t\352te de noeud !")}
+	document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + compteur;	
 	document.getElementById("affichage").innerHTML = score;
-}
+};
 
 function point() {
 	document.getElementById("affichage").innerHTML = score += compteur; 
