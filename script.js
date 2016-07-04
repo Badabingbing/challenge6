b9f4d540a1a3a53a51153c1976c3090bfb06ebd5
Valentin <valentinsusset@hotmail.com>
2016-07-04 23:10:38 +0200

    js fini

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var score = 0;
 var compteur = 1;
+var compteurauto = 0;
 
 function prix(){
 	return 50*Math.pow(2,compteur-1);
@@ -8,7 +9,7 @@ function prix(){
 function incrementer() {
 	if (score >= prix()) {score -= prix(); compteur ++}
 	else {alert("Il te faut au moins " + prix() + " points t\352te de noeud !")}
-	document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + compteur + "Co\373t du prochain achat " + prix();
+	document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + compteur + "<br>Co\373t du prochain achat : " + prix();
 	document.getElementById("affichage").innerHTML = score;
 };
 
@@ -16,3 +17,21 @@ function point() {
 	document.getElementById("affichage").innerHTML = score += compteur; 
 };
 
+function prixauto(){
+	return 500*Math.pow(2,compteurauto);
+}
+
+function clicker() {
+	setInterval(function() {document.getElementById("button").click();}, 1000);
+}
+
+function autoclick() {
+if (score>= prixauto()) {
+	score -=prixauto(); compteurauto ++;
+	if (score >= 200) {
+	clicker();
+	}
+} else {alert("Clique encore, cette fois c'est la bonne")};
+document.getElementById("autoclicker").innerHTML = "autoclicker x" + compteurauto + "<br>Co\373t du prochain achat : " + prixauto();
+document.getElementById("affichage").innerHTML = score;	
+};
