d6f2e539d6bc1cf426d713322f543f166d07dbce
Valentin <valentinsusset@hotmail.com>
2016-07-04 17:26:06 +0200

    etape 10

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var score = 0;
 var compteur = 1;
 
+function prix(){
+	return 50*Math.pow(2,compteur-1);
+}
+
 function incrementer() {
-	if (score >= 50) {score -= 50; compteur ++}
-	else {alert("Il te faut au moins 50 points t\352te de noeud !")}
-	document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + compteur;	
+	if (score >= prix()) {score -= prix(); compteur ++}
+	else {alert("Il te faut au moins " + prix() + " points t\352te de noeud !")}
+	document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + compteur + "Co\373t du prochain achat " + prix();
 	document.getElementById("affichage").innerHTML = score;
 };
 
 function point() {
 	document.getElementById("affichage").innerHTML = score += compteur; 
 };
-
-
-
-
 
